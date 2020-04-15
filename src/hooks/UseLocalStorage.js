75bf4c7a363f87386ede5d5feee81550dc6fc346
Matthew Taylor <majikpig@gmail.com>
2020-04-14 22:16:52 -0400

    select coins by choosing icons

--- src/hooks/UseLocalStorage.js
+++ src/hooks/UseLocalStorage.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useLocalStorage = (key, initialValue) => {
+function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
     const item = window.localStorage.getItem(key);
     return item ? JSON.parse(item) : initialValue;
--- src/index.js
+++ src/index.js
@@ -5,23 +5,39 @@ import axios from "axios";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
+import useLocalStorage from './hooks/UseLocalStorage';
+
 import "./styles.scss";
+
+const initialCoins = ['btc', 'eth', 'bch', 'ltc'];
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [activeCoins, setActiveCoins] = useLocalStorage('activeCoins',initialCoins);
+
+  const setCoinActive = (symbol, activity) => {
+    console.log(activity);
+    if (activity===true) {
+      setActiveCoins([...activeCoins, symbol]);
+    } else {
+      setActiveCoins(activeCoins.filter(coin=>coin!==symbol));
+    }
+  }
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
+      .then(res => {
+        setCoinData(res.data);
+      })
       .catch(err => console.log(err));
   }, []);
   return (
     <div className="App">
-      <Navbar />
-      <Charts coinData={coinData} />
+      <Navbar coinData={coinData} activeCoins={activeCoins} setCoinActive={setCoinActive} />
+      <Charts coinData={coinData} activeCoins={activeCoins} />
     </div>
   );
 };
